7fadfb174b867c80e70dc1d5f63d71faf5bcdaaf
nayan <www.xil187@gmail.com>
2024-08-18 20:19:16 +0530

    Numbers&Strings

---
+++ Numbers/src/Numbers.java
@@ -0,0 +1,9 @@
+public static void main(String[] args)
+{
+    double value = -234.898;
+
+    System.out.println(STR."absolute value \{Math.abs(value)}");
+    System.out.println(STR."ceil value \{Math.ceil(value)}");
+    System.out.println(STR."floor value \{Math.floor(value)}");
+
+}
